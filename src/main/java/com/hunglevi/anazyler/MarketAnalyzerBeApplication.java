7fb4513ed8a3f9d05a57db129e63cbec284eb1e9
HungVuLong <vulong13062005@gmail.com>
2026-01-21 21:39:39 +0700

    starter

---
+++ src/main/java/com/hunglevi/anazyler/MarketAnalyzerBeApplication.java
@@ -0,0 +1,13 @@
+package com.hunglevi.anazyler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketAnalyzerBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarketAnalyzerBeApplication.class, args);
+	}
+
+}
